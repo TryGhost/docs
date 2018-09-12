700a7417d42a3f017a2e18192408aa5ccc81db91
John O'Nolan <john@onolan.org>
2018-09-12 23:52:07 +0200

    Let’s do some navigation that’s all I’m saying

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,22 @@
-import PropTypes from 'prop-types'
 import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from "gatsby"
+
 import Logo from "components/logo"
 
-const Header = ({ siteTitle }) => (
-    <header className="h5 shadow-2">
+const Header = () => (
+    <header className="shadow-2">
         <nav className="flex flex-auto flex-nowrap items-start justify-between">
-            <div className="">
-                <a className="" href="https://help.ghost.org">
-                    <Logo /> {/* TODO: Prop to pass in height value? */}
-                </a>
+            <div className="flex flex-auto flex-nowrap items-center">
+                <Link className="pa2" to="/"><Logo /> {/* TODO: Prop to pass in height value? */}</Link>
+                <Link className="pa2" to="/setup/">Setup</Link>
+                <Link className="pa2" to="/concepts/introduction/">Core Concepts</Link>
+                <Link className="pa2" to="/tutorials/">Tutorials</Link>
+                <Link className="pa2" to="/api/">API</Link>
+                <Link className="pa2" to="/integrations/">Integrations</Link>
+                <Link className="pa2" to="/faq/">FAQ</Link>
             </div>
-            <div className="">
+            <div className="flex flex-auto flex-nowrap justify-end items-center">
                 <div className="relative">
                     <input name="foo" type="text" className="db pa2 ba midgrey f8 fw4 br3 whitney form-text b--lightgrey" placeholder="Placeholder"></input>
                 </div>
