1731059c84d891f21b403ac1da65cb44be7af4a8
Zimo <peter.zimon@gmail.com>
2018-09-14 12:52:32 +0200

    Moving default layout to a react component

--- src/components/layouts/default.js
+++ src/components/layouts/default.js
@@ -8,9 +8,13 @@ import Header from './partials/header'
 import '../custom.css'
 import 'ghost-spirit/public/spirit-brand.css'
 
-const DefaultLayout = ({ children }) => (
-    <StaticQuery
-        query={graphql`
+class DefaultLayout extends React.Component {
+
+    render() {
+        const children = this.props.children
+        return (
+            <StaticQuery
+                query={ graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
@@ -19,23 +23,55 @@ const DefaultLayout = ({ children }) => (
         }
       }
     `}
-        render = {data => (
-      <>
-        <Helmet>
-            <html lang="en" className="fs-base" />
-            <title>{ data.site.siteMetadata.title }</title>
-            <meta name="description" content="Ghost Docs" />
-            <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6076934/7558352/css/fonts.css" />
-            <body class="flex flex-column whitney f-default fw4 middarkgrey readability" />
-        </Helmet>
-        <Header />
-        <main className="bg-grey">
-            {children}
-        </main>
-      </>
-        )}
-    />
-)
+                render={ data => (
+                    <>
+                        <Helmet>
+                            <html lang="en" className="fs-base" />
+                            <title>{ data.site.siteMetadata.title }</title>
+                            <meta name="description" content="Ghost Docs" />
+                            <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6076934/7558352/css/fonts.css" />
+                            <body class="flex flex-column whitney f-default fw4 middarkgrey readability" />
+                        </Helmet>
+                        <Header />
+                        <main className="bg-grey">
+                            { children }
+                        </main>
+                    </>
+                ) }
+            />
+        )
+    }
+
+}
+
+// const DefaultLayout = ({ children }) => (
+//     <StaticQuery
+//         query={graphql`
+//       query SiteTitleQuery {
+//         site {
+//           siteMetadata {
+//             title
+//           }
+//         }
+//       }
+//     `}
+//         render = {data => (
+//       <>
+//         <Helmet>
+//             <html lang="en" className="fs-base" />
+//             <title>{ data.site.siteMetadata.title }</title>
+//             <meta name="description" content="Ghost Docs" />
+//             <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6076934/7558352/css/fonts.css" />
+//             <body class="flex flex-column whitney f-default fw4 middarkgrey readability" />
+//         </Helmet>
+//         <Header />
+//         <main className="bg-grey">
+//             {children}
+//         </main>
+//       </>
+//         )}
+//     />
+// )
 
 DefaultLayout.propTypes = {
     children: PropTypes.node.isRequired,
