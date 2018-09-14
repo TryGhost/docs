bf00cb1fe22e6e175aaa69457fd9e508c4de8855
Zimo <peter.zimon@gmail.com>
2018-09-14 15:04:18 +0200

    Adding Spirit instructions to Readme + some cleanup

--- README.md
+++ README.md
@@ -34,6 +34,47 @@ See --> [API docs repo](https://github.com/TryGhost/docs-api)
 
 ### Concepts & setup
 
+## Spirit
+Ghost Docs uses [Spirit/Brand](https://github.com/TryGhost/Spirit) as its design system. For development, it's recommended to `yarn link` the local Spirit repo to docs ([`yarn link` how to](https://yarnpkg.com/lang/en/docs/cli/link/)).
+
+### Using Spirit
+
+There are multiple ways you can use Spirit:
+
+#### 1. Inline Spirit (Tachyons) CSS classes
+You can use Spirit CSS classes directly in `className` attribute. Typically for non-reusable, custom components where Spirit classes are sufficiently let you style the component.
+
+_Example:_
+```
+<div className="flex justify-between pa10"></div>
+```
+
+#### 2. SpiritStyle
+- `import { SpiritStyle } from '/src/components/spirit-brand/spirit-styles.js'` in components
+- The properties of `SpiritStyle` object are predefined combinations of Spirit CSS classes. Use it for all the reusable styles (e.g. links, headings etc.).
+- _TODO: needs to be outsourced to a gatsby plugin or external react module. (The Ghost admin client is using the [ember-cli-ghost-spirit](https://github.com/TryGhost/ember-cli-ghost-spirit) addon to access Spirit/Product helpers.)_
+
+_Example:_
+```
+<h1 className={ SpiritStyle.heading.h1 }></h1> <!-- Returns "f1 fw5 bb b--lightgrey pb3 mb7 mt10" -->
+```
+
+#### 3. Styled Components + CSS classes or SpiritStyle
+Use [styled-components](https://next.gatsbyjs.org/docs/styled-components/) combined with Spirit styles to inject custom CSS within the context of the given component.
+
+_Example:_
+```
+const Header = () => (
+    <Container>...</Container>
+)
+
+const Container = styled.div.attrs({
+        className: { SpiritStyle.box } " shadow-2"
+})`
+    margin: 10px;
+`
+```
+
 ## Ghost Content
 
 --> https://docs-2.ghost.io
--- src/components/layouts/default.js
+++ src/components/layouts/default.js
@@ -16,9 +16,8 @@ import 'ghost-spirit/public/spirit-brand.css'
 import 'prismjs/themes/prism.css'
 
 class DefaultLayout extends React.Component {
-
     componentDidMount() {
-        Prism.highlightAll();
+        Prism.highlightAll()
     }
 
     render() {
@@ -41,7 +40,7 @@ class DefaultLayout extends React.Component {
                             <title>{ data.site.siteMetadata.title }</title>
                             <meta name="description" content="Ghost Docs" />
                             <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6076934/7558352/css/fonts.css" />
-                            <body class="flex flex-column whitney f-default fw4 middarkgrey readability" />
+                            <body className="flex flex-column whitney f-default fw4 middarkgrey readability" />
                         </Helmet>
                         <Header />
                         <main className="bg-grey">
@@ -52,37 +51,7 @@ class DefaultLayout extends React.Component {
             />
         )
     }
-
 }
-
-// const DefaultLayout = ({ children }) => (
-//     <StaticQuery
-//         query={graphql`
-//       query SiteTitleQuery {
-//         site {
-//           siteMetadata {
-//             title
-//           }
-//         }
-//       }
-//     `}
-//         render = {data => (
-//       <>
-//         <Helmet>
-//             <html lang="en" className="fs-base" />
-//             <title>{ data.site.siteMetadata.title }</title>
-//             <meta name="description" content="Ghost Docs" />
-//             <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6076934/7558352/css/fonts.css" />
-//             <body class="flex flex-column whitney f-default fw4 middarkgrey readability" />
-//         </Helmet>
-//         <Header />
-//         <main className="bg-grey">
-//             {children}
-//         </main>
-//       </>
-//         )}
-//     />
-// )
 
 DefaultLayout.propTypes = {
     children: PropTypes.node.isRequired,
