c06740da8f46419bc152411459d205df3933ced9
Hannah Wolfe <erisds@gmail.com>
2018-09-14 14:55:59 +0100

    Prevent gatspocolypse - readd scripts to page
    - This is my first pass at a solution. It's not pretty.
    - The idea is to post-process the page, look for any script tags, and add them to the head.
    - This _works_ but its weird to do this in the browser when it should be possible to do it as part of the render loop

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,29 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+/**
+ * Trust All Scripts
+ *
+ * This is a dirty little script for iterating over script tags
+ * and adding them to the document head.
+ *
+ * This works for any script that then injects content into the page
+ * via ids/classnames etc.
+ */
+const trustAllScripts = () => {
+    var scriptNodes = document.querySelectorAll(`main script`)
+
+    scriptNodes.forEach((node) => {
+        // @TODO do the same for inline scripts?
+        if (node.attributes.src) {
+            var s = document.createElement(`script`)
+            s.type = `text/javascript`
+            s.src = node.attributes.src.value
+            document.getElementsByTagName(`head`)[0].appendChild(s)
+        }
+    })
+}
+
+exports.onRouteUpdate = function () {
+    trustAllScripts()
+}
