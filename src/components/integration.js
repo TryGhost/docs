5f502abaa303963b27041979361baf131a43033a
John O'Nolan <john@onolan.org>
2018-09-24 17:24:15 +0200

    Micro design adjustments

--- src/components/integration.js
+++ src/components/integration.js
@@ -7,8 +7,8 @@ const Integration = (props) => {
     const url = `/integrations/${post.slug}/`
 
     return (
-        <Link className="gh-integration-card flex-grow-0 flex flex-column justify-center items-center w30 h30 ma5 tc link darkgrey shadow-2-on-white br5" to={url}>
-            <div className="flex justify-center items-center h10 w13 mb3">
+        <Link className="gh-integration-card flex-grow-0 flex flex-column justify-center items-center w30 h30 ma5 pa3 tc link darkgrey bg-white shadow-2 br5" to={url}>
+            <div className="flex justify-center items-center h10 w13 mt1 mb2">
                 <img className="w-100 h-100" style={{ objectFit: `contain` }} src={post.feature_image} alt={post.title} />
             </div>
             <div className="f8">{post.title}</div>
--- src/components/layouts/partials/navbar.js
+++ src/components/layouts/partials/navbar.js
@@ -40,7 +40,7 @@ class NavBar extends React.Component {
                 <div className="flex flex-auto flex-nowrap items-center f8">
                     <div className="flex items-center pt3 pb3 mr12 nudge-bottom--2">
                         <a href="https://ghost.org" className="nudge-top--3"><Logo theme={ this.props.theme } /></a>
-                        <Link className={ theme.docsTitleClass + ` relative ma0 ml4 pa0 pl4 f6 lh-1-5 fw4 link gh-nav-logo-suffix` } to="/">Docs</Link>
+                        <Link className={theme.docsTitleClass + ` gh-nav-logo-suffix relative ma0 ml4 pa0 pl4 f6 lh-1-5 fw4 link nudge-top--1` } to="/">Docs</Link>
                     </div>
                     <Link className={ theme.menuItem + ` mr7` } to="/setup/">Setup</Link>
                     <Link className={ theme.menuItem + ` mr7` } to="/concepts/introduction/">Core Concepts</Link>
