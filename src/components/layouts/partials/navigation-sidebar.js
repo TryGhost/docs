ad9d420fa97e7fd875625c796c5851e3ba7f2b2d
Zimo <peter.zimon@gmail.com>
2018-09-25 19:14:43 +0200

    Harmonizing sidebar fonts

--- src/components/layouts/partials/navigation-sidebar.js
+++ src/components/layouts/partials/navigation-sidebar.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { Spirit } from '../../spirit-styles'
 
 const NavSidebar = () => (
     <>
-        <nav className="mr5 miw50 f7">
-            <h3 className={ Spirit.h6 }>Setup</h3>
-            <ul className="ma0 pa0 list mt4">
-                <li className="mb6"><Link to="" className="link midgrey">What is Ghost?</Link></li>
+        <nav className="mr5 miw50">
+            <h3 className="f8 ttu fw6 pa0 ma0 measure-0-4 pb2">Setup</h3>
+            <ul className="ma0 pa0 list mt4 f8">
+                <li className="mb4"><Link to="" className="link midgrey">What is Ghost?</Link></li>
                 <li className="mb6">
                     <h4 className="fw4"><Link to="" className="link midgrey">Setting up a site</Link></h4>
                     <ul className="list ma0 pa0 ml6">
@@ -19,7 +18,7 @@ const NavSidebar = () => (
                 <li className="mb6">
                     <h4 className="fw4"><Link to="" className="link midgrey">Community packages</Link></h4>
                     <ul className="list ma0 pa0 ml6">
-                        <li className="mb3 lh-1-4"><Link to="" className="link midgrey">Docker</Link></li>
+                        <li className="mb3 lh-1-4"><Link to="" className="link midgrey">Docker</Link></li>                        
                     </ul>
                 </li>
                 <li className="mb6">
--- src/components/layouts/partials/toc.js
+++ src/components/layouts/partials/toc.js
@@ -5,15 +5,15 @@ import { Link } from 'gatsby'
 const TOC = () => (
     <>
         <nav className="pl10 miw50 nr5 sticky top-25">
-            <h3 className="f7 fw4 measure-0-4 ma0 pa0 ttu pb2 bb b--whitegrey lightgrey">On this page</h3>
+            <h3 className="f8 fw4 measure-0-4 ma0 pa0 ttu pb2 fw6">On this page</h3>
             <ul className="ma0 pa0 list mt4 f8">
                 <li className="mb4"><Link to="" className="link midgrey">Overview</Link></li>
                 <li className="mb4"><Link to="" className="link midgrey">Errors</Link></li>
                 <li className="mb4"><Link to="" className="link midgrey">Pagination</Link></li>
                 <li className="mb4"><Link to="" className="link midgrey">Parameters</Link></li>
-                <li className="mb8">
+                <li className="mb6">
                     <h4 className="fw4 mb4"><Link to="" className="link midgrey">Client authentication</Link></h4>
-                    <ul className="list ma0 pa0 ml7">
+                    <ul className="list ma0 pa0 ml6">
                         <li className="mb3 lh-1-4"><Link to="" className="link blue fw6">Making requests</Link></li>
                         <li className="mb3 lh-1-4"><Link to="" className="link midgrey">Available clients</Link></li>
                         <li className="mb3 lh-1-4"><Link to="" className="link midgrey">Client restrictions</Link></li>
