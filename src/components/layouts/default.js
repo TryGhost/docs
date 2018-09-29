f83cad6f6621c719fad9f427d60e2b33e02ba707
John O'Nolan <john@onolan.org>
2018-09-29 22:31:27 +0200

    Add valid meta placeholder

--- src/components/layouts/default.js
+++ src/components/layouts/default.js
@@ -27,6 +27,8 @@ class DefaultLayout extends React.Component {
             <>
                 <Helmet>
                     <html lang="en" className="fs-base" />
+                    <title>Ghost</title> /* TODO: This should be dynamic */
+                    <meta name="description" content="Ghost documentation" />  /* TODO: This should be dynamic */
                     <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6076934/7558352/css/fonts.css" />
                     <body className={ this.props.bodyClass + ` flex flex-column whitney f7 fw4 middarkgrey readability`} />
                 </Helmet>
