4dbee44609865a079f5ca6a64c83352172bff7da
Aileen Nowak <AileenCGN@gmail.com>
2018-10-05 11:43:13 +0700

    Sidebar: expand sidebar items on click

--- src/components/global/navigation-sidebar.js
+++ src/components/global/navigation-sidebar.js
@@ -2,8 +2,31 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
-// TODO: find a way to prevent duplicated active links. Propably passing states to the
-// parent component.
+/**
+ * Takes a path string and created different path RegExps to test against
+ * @param  {String} path e. g. `/api/context/something/`
+ * @return {Array} with combinations of this path as RegExp, e.g. `/api/$`, `/api/context/$`, `/api/context/something/$`
+ */
+function getSidebarListRegex(path) {
+    const sidebarlistsRegexToExpand = []
+    let pathArray = path.split(`/`)
+    let pathRegex
+
+    pathArray = pathArray.filter(path => (path.length > 0))
+
+    pathArray.forEach((path, i) => {
+        if (i < 1) {
+            pathRegex = `/${path}/`
+        } else {
+            pathRegex += `${path}/`
+        }
+        sidebarlistsRegexToExpand.push(new RegExp(`${ pathRegex }$`, `i`))
+    })
+
+    return sidebarlistsRegexToExpand
+}
+
+// TODO: find a way to prevent duplicated active links.
 class SidebarLink extends React.Component {
     render() {
         if (this.props.link.match(/^\s?http(s?)/gi)) {
@@ -29,7 +52,6 @@ class SidebarList extends React.Component {
         super(props)
         this.state = {
             sidebarListClasses: `dn`,
-            activeLinkId: ``,
             linkClasses: `midgrey`,
         }
         this.extendSidebar = this.extendSidebar.bind(this)
@@ -37,9 +59,7 @@ class SidebarList extends React.Component {
     }
 
     setActiveLink() {
-        this.setState((state, props) => {
-            return { activeLinkId: props.id, linkClasses: `blue fw6` }
-        })
+        this.setState({ linkClasses: `blue fw6` })
     }
 
     extendSidebar() {
@@ -47,18 +67,17 @@ class SidebarList extends React.Component {
     }
 
     render() {
-        let level = this.props.newLevel || 1
+        let level = this.props.level || 1
         const hasChildren = this.props.item.items && this.props.item.items.length
 
         if (hasChildren) {
-            // CASE: the section title does not have a link, but it has child items, so we take the
-            // first link we find from the child item
+            // A section can not have a link on its own. In this case, we grab the
+            // link of the first nested item
             const autoLink = this.props.item.link || this.props.item.items[0].link
             const childrenLevel = level += 1
-            const sideBarClass = this.props.expandSidebar || this.state.sidebarListClasses
 
             return (
-                <li className="mb6" onClick={this.extendSidebar}>
+                <li className="mb6">
                     <h4 className="fw4">
                         <SidebarLink
                             link={autoLink}
@@ -66,15 +85,14 @@ class SidebarList extends React.Component {
                             linkClasses={this.state.linkClasses}
                         />
                     </h4>
-                    <ul id={this.props.id} className={`list ma0 pa0 ml6 ${this.state.activeLinkId === this.props.id ? `db` : sideBarClass}`}>
+                    <ul className={`list ma0 pa0 ml6 ${this.state.sidebarListClasses}`}>
                         {this.props.item.items.map((nestedLink, i) => (
                             <SidebarList
                                 key={i}
-                                id={`${this.props.id}-${i}-${nestedLink.title.toLowerCase()}`}
                                 item={nestedLink}
                                 location={this.props.location}
-                                newLevel={childrenLevel}
-                                expandSidebar={`db`}
+                                level={childrenLevel}
+                                expandedSidebarLists={this.props.expandedSidebarLists}
                             />
                         ))}
                     </ul>
@@ -94,57 +112,54 @@ class SidebarList extends React.Component {
     }
 
     componentDidMount() {
-        let activeLinkRegex
-        // Someone will probably kill me for this, but the state will
-        // not get updated immediately, so need this temp helper here
-        // TODO: make this pretty and React.js conform
-        let activeLinkTemp
-
         // First, find the currently active link
         if (this.props.location.pathname === this.props.item.link) {
-            activeLinkTemp = this.props.id
-            activeLinkRegex = new RegExp(`^${this.props.id}`)
             this.setActiveLink()
         }
 
-        if (activeLinkTemp && activeLinkTemp.match(activeLinkRegex)) {
-            this.extendSidebar()
-        }
+        this.props.expandedSidebarLists.forEach((pathRegex) => {
+            // Test against our list of positive urls. When the current link
+            // matches any item from the list, the nested list items should be visible
+            if (this.props.item.link.match(pathRegex)) {
+                this.extendSidebar()
+            }
+        })
     }
 }
 
 SidebarList.propTypes = {
     item: PropTypes.object.isRequired,
     location: PropTypes.object.isRequired,
+    expandedSidebarLists: PropTypes.array.isRequired,
+    level: PropTypes.number,
 }
 
-SidebarList.propTypes = {
-    item: PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired,
-    level: PropTypes.string,
-}
-
-// TODO: show only first level links by default, expand on click
 // TODO: create special treatment options for titles that have a `*`
-
+// TODO: different styles for second and third level items
 class SidebarNav extends React.Component {
     render() {
         const { sidebar, location } = this.props
         const [sidebarfile] = sidebar ? require(`../../data/sidebars/${sidebar}.yaml`) : []
+        const sidebarlistsRegexToExpand = getSidebarListRegex(location.pathname)
 
         if (!sidebarfile) {
             return null
         }
 
         return (
-            <>
-                <nav className="mr5 miw50">
-                    <h3 className="f8 ttu fw6 pa0 ma0 measure-0-4 pb2">{sidebarfile.title}</h3>
-                    <ul className="ma0 pa0 list mt4 f8">
-                        {sidebarfile.items.map((item, i) => <SidebarList key={i} id={`${i}-${item.title.toLowerCase()}`} item={item} location={location} />)}
-                    </ul>
-                </nav>
-            </>
+            <nav className="mr5 miw50">
+                <h3 className="f8 ttu fw6 pa0 ma0 measure-0-4 pb2">{sidebarfile.title}</h3>
+                <ul className="ma0 pa0 list mt4 f8">
+                    {sidebarfile.items.map((item, i) => (
+                        <SidebarList
+                            key={i}
+                            item={item}
+                            location={location}
+                            expandedSidebarLists={sidebarlistsRegexToExpand}
+                        />
+                    ))}
+                </ul>
+            </nav>
         )
     }
 }
@@ -157,5 +172,5 @@ SidebarNav.propTypes = {
 }
 
 SidebarNav.defaultProps = {
-    location: { path: `/` },
+    location: { pathname: `/` },
 }
