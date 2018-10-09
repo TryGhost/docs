9341e3462073d0e1b38ddc1df184d280b8109ae5
Zimo <peter.zimon@gmail.com>
2018-10-09 16:14:32 +0200

    Using grid for setup

--- src/pages/setup.js
+++ src/pages/setup.js
@@ -4,6 +4,7 @@ import Icon from '../components/global/icon'
 
 import { Link } from 'gatsby'
 import { Spirit } from '../components/spirit-styles'
+import Box from '../components/layouts/partials/box';
 
 // TODO: structured data
 const SetupIndexPage = () => (
@@ -16,52 +17,24 @@ const SetupIndexPage = () => (
         </section>
 
         <div className={ Spirit.page.l }>
-
-            <div className={ `pt-vw7 pt-vw2-ns` }>
-                <h2 className={ Spirit.h2 + `col-12` }>What is Ghost?</h2>
-                <p className={ Spirit.excerpt + `col-8 mt2 measure` }>Sunt duis ad aliqua aute do ex excepteur enim eiusmod nostrud occaecat dolor nisi. Qui nulla exercitation est minim commodo mollit quis incididunt. Est excepteur sit deserunt quis magna do irure quis. Fugiat id qui aliquip ullamco sunt. </p>
-                <p className="col-12 mt5"><Link to="/setup/what-is-ghost/" className={ Spirit.excerpt + `blue link fw5 flex items-center` }>Learn more <Icon name="arrow-right" className="h4 fill-blue nl2" /></Link></p>
-            </div>
-
-            <section className="grid gutter-2">
-                <h3 className={ Spirit.h4 + `col-12 mt15 mb2` }>I want to setup a site</h3>
-                <div className="col-4">
-                    <Link to="/setup/ghost-pro/" className="link midgrey">
-                        <div className="shadow-2 bg-white br4 pa4 pt20 pb20 tc mb5">
-                            <h4 className={ Spirit.h5 + `lightgrey`}>Ghost(Pro)</h4>
-                        </div>
-                    </Link>
-                </div>
-                <div className="col-4">
-                    <Link to="/install/ubuntu/" className="link midgrey">
-                        <div className="shadow-2 bg-white br4 pa4 pt20 pb20 tc mb5">
-                            <h4 className={ Spirit.h5 + `lightgrey` }>Ubuntu</h4>
-                        </div>
-                    </Link>
-                </div>
-                <div className="col-4">
-                    <Link to="/install/docker/" className="link midgrey">
-                        <div className="shadow-2 bg-white br4 pa4 pt20 pb20 tc mb5">
-                            <h4 className={Spirit.h5 + `lightgrey`}>Docker</h4>
-                        </div>
-                    </Link>
-                </div>
-                <div className="col-4">
-                    <Link to="/install/local/" className="link midgrey">
-                        <div className="shadow-2 bg-white br4 pa4 pt20 pb20 tc mb5">
-                            <h4 className={Spirit.h5 + `lightgrey`}>Local Install</h4>
-                        </div>
-                    </Link>
-                </div>
-                <div className="col-4">
-                    <Link to="/install/source/" className="link midgrey">
-                        <div className="shadow-2 bg-white br4 pa4 pt20 pb20 tc mb5">
-                            <h4 className={Spirit.h5 + `lightgrey`}>Install from Source</h4>
-                        </div>
-                    </Link>
-                </div>
+            <section className="grid-12 gutter-row-20 gutter-32-l">
+                <h3 className={ Spirit.h4 + `col-12 mt15 mb2 middarkgrey` }>I want to setup a site</h3>
+                <Box className="col-12 col-4-ns pa12 tc tdn" to="/setup/ghost-pro/">
+                    <h4 className={ Spirit.h5 + `lightgrey` }>Ghost(Pro)</h4>
+                </Box>
+                <Box className="col-12 col-4-ns pa12 tc tdn" to="/install/ubuntu/">
+                    <h4 className={ Spirit.h5 + `lightgrey` }>Ubuntu</h4>
+                </Box>
+                <Box className="col-12 col-4-ns pa12 tc tdn" to="/install/docker/">
+                    <h4 className={ Spirit.h5 + `lightgrey` }>Docker</h4>
+                </Box>
+                <Box className="col-12 col-4-ns pa12 tc tdn" to="/install/local/">
+                    <h4 className={ Spirit.h5 + `lightgrey` }>Local install</h4>
+                </Box>
+                <Box className="col-12 col-4-ns pa12 tc tdn" to="/install/source/">
+                    <h4 className={ Spirit.h5 + `lightgrey` }>Install from Source</h4>
+                </Box>
             </section>
-
         </div>
 
     </Layout>
