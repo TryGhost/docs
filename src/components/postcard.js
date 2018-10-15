35fb74941ae7fe90b5c0c7576eb3b76ba7dec872
Zimo <peter.zimon@gmail.com>
2018-10-15 22:15:23 +0200

    Working on tutorials/FAQ pages

--- src/components/postcard.js
+++ src/components/postcard.js
@@ -5,28 +5,36 @@ import Link from 'gatsby-link'
 // import Authors from './authors'
 import { Spirit } from './spirit-styles'
 import getPostExcerpt from '../utils/post-excerpt'
+import Box from '../components/layouts/partials/box'
 
 const PostCard = ({ post, className }) => {
-    const tag = post.primaryTag ? post.primaryTag.name : (post.tags ? post.tags[0].name : `Untagged`)
+    // const tag = post.primaryTag ? post.primaryTag.name : (post.tags ? post.tags[0].name : `Untagged`)
     const url = `/tutorials/${post.slug}/`
     const excerpt = getPostExcerpt(post)
 
     return (
-        <article className={ className + ` bg-white br4 shadow-2 pa10 pa8 flex flex-column justify-between flex-third relative box-hover-test` }>
-            <Link to={ url } className="tdn">
+        <Box to={ url } className={ className + ` pa10 pa8 flex flex-column justify-between flex-third relative box-hover-test faq-post-card-min-height tdn` }>
+            <div>
                 <header>
-                    { post.featured ? <span className="bg-green-l2 pa1 f-supersmall fw5 dib measure-0-2 mr2 white br-pill pl2 pr2 nl2">Featured</span> : null }
-                    <span className="midgrey f8">{ tag }</span>
-                    <h2 className={ Spirit.h3 + `middarkgrey-d1` + (post.featured ? ` mt2` : ` mt4`) }>{ post.title }</h2>
+                    {/* <span className="midgrey f8">{ tag }</span> */}
+                    <h2 className={ Spirit.h3 + `darkgrey` + (post.featured ? ` mt0` : ` mt0`) }>{ post.title }</h2>
                 </header>
-                { excerpt ? <section className={ Spirit.p + `middarkgrey-l2 mt4` }>{ excerpt }</section> : null }
-            </Link>
+                { excerpt ? <section className={ Spirit.p + `middarkgrey mt4` }>{ excerpt }</section> : null }
+            </div>
 
             <footer className="flex pt2 mt6 content-end">
                 {/* <Authors authors={ post.authors } /> */}
-                <span className="f8 dib measure-0-2 lightgrey">{ post.published_at_pretty }</span>
+                {/* <span className="f8 dib measure-0-2 lightgrey">{ post.published_at_pretty }</span> */}
+                { post.featured ? <span className="bg-tutorial-color pa1 f8 fw5 dib measure-0-2 mr2 white br2 pl3 pr3">Featured</span> : null }
+                { post.tags ? post.tags.map((item, i) => {
+                    if (item.name.match(/^#/)) {
+                        return null
+                    } else {
+                        return <span key={ i } className="bg-midgrey dib word-nowrap pt1 pb1 pl2 pr2 white br2 f8">{ item.name }</span> 
+                    }
+                }) : null }
             </footer>
-        </article>
+        </Box>
     )
 }
 
