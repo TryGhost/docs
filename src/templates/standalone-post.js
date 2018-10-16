ffc1ac5f6b7de575ccc5432e53d54b9e124afc15
Zimo <peter.zimon@gmail.com>
2018-10-16 17:23:01 +0200

    Working on tutorials post design

--- src/templates/standalone-post.js
+++ src/templates/standalone-post.js
@@ -23,7 +23,7 @@ class Tutorial extends React.Component {
                     <div className={ Spirit.page.xl + `pb15` }>
                         <article className="mw-content center pa15 pb10">
                             <h1 className={ Spirit.h1 }>{ post.title }</h1>
-                            <section className="post-content" dangerouslySetInnerHTML={ { __html: post.html } } />
+                            <section className="post-content tutorial-content" dangerouslySetInnerHTML={ { __html: post.html } } />
                             {/* <Authors authors={ post.authors } /> */}
                         </article>
 
