b62a8ec11128bafad87bcfa3c730a92eb9ceac40
Aileen Nowak <AileenCGN@gmail.com>
2018-10-17 18:18:12 +0700

    Make form work.... now?

--- src/components/global/feedback-form.js
+++ src/components/global/feedback-form.js
@@ -31,7 +31,7 @@ class FeedbackForm extends React.Component {
         this.setState({ [e.target.name]: e.target.value })
     }
 
-    handleSubmit(e) {
+    handleSubmit = (e) => {
         e.preventDefault()
         const form = e.target
         fetch(`/`, {
@@ -74,16 +74,17 @@ class FeedbackForm extends React.Component {
                     <form
                         name="feedback"
                         method="post"
+                        action="#"
                         data-netlify="true"
                         data-netlify-honeypot="bot-field"
                         onSubmit={this.handleSubmit}
                     >
-                        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                        <input type="hidden" name="form-name" value="feedback" />
+                        {/* The `form-name` hidden field is required to support form submissions without JavaScript
+                        <input type="hidden" name="form-name" value="feedback" /> */}
                         <p hidden>
                             <label>
                                 Don’t fill this out:{` `}
-                                <input name="bot-field" onChange={this.handleChange} />
+                                <input name="bot-field" type="hidden" onChange={this.handleChange} />
                             </label>
                         </p>
                         <div className="flex items-center">
