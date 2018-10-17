155ee3bc7c44f144dc07f1a781e1a14131d20d44
Hannah Wolfe <erisds@gmail.com>
2018-10-17 21:33:47 +0100

    Full indexing of ghost+md to algolia w/ urls

--- src/utils/algolia-queries.js
+++ src/utils/algolia-queries.js
@@ -1,21 +1,82 @@
-const { allGhostPosts } = require(`./node-queries`)
-const { ghostQueryConfig } = require(`./query-config`)
+const { allGhostPosts, allMarkdownPosts } = require(`./node-queries`)
+const { ghostQueryConfig, markdownQueryConfig } = require(`./query-config`)
+const urlUtils = require('./urls')
 
-const algoliaFields = `
-    objectID:id,
-    slug,
-    title,
-    html,
-    tags {
-        slug
-        name
-    }
+const algoliaGhostFields = `
+    objectID:id
+    slug
+    title
+    html
 `
 
-module.exports = ghostQueryConfig.map(({ tag, indexName }) => {
+const algoliaMarkdownFields = `
+    objectID:id
+    fields {
+        slug
+        section
+    }
+    frontmatter {
+        title
+    }
+    html
+`
+
+const chunkString = (str, length) => str.match(new RegExp(`(.|[\r\n]){1,` + length + `}`, `g`))
+
+const transformer = (chunksTotal, node) => {
+    const htmlChunks = chunkString(node.html, 5000)
+    const record = node;
+    const recordChunks = htmlChunks.reduce((recordChunksTotal, htmlChunksItem, idx) => {
+        return [
+            ...recordChunksTotal,
+            { ...record, ...{ html: htmlChunksItem }, objectID: `${node.objectID}_${idx}` }
+        ];
+    }, []);
+
+    return [...chunksTotal, ...recordChunks];
+}
+
+let ghostQueries = ghostQueryConfig.map(({ tag, section, indexName }) => {
     return {
-        query: allGhostPosts(tag, algoliaFields),
+        query: allGhostPosts(tag, algoliaGhostFields),
         indexName,
-        transformer: ({ data }) => data.allGhostPost.edges.map(({ node }) => node),
+        transformer: ({ data }) => data
+            .allGhostPost.edges
+            .map(({ node }) => {
+                // @TODO is there some other way to do this?!
+                node.section = section
+                node.url = urlUtils.urlForGhostPost(node, section)
+                return node
+            })
+            .reduce(transformer, []),
     }
 })
+
+let mdQueries = markdownQueryConfig.map(({ section, indexName }) => {
+    return {
+        query: allMarkdownPosts(section, algoliaMarkdownFields),
+        indexName,
+        transformer: ({ data }) => data
+            .allMarkdownRemark.edges
+                .map(({ node }) => {
+                    // Flatten fields
+                    node.slug = node.fields.slug
+                    node.section = node.fields.section
+                    node.title = node.frontmatter.title
+                    // @TODO make this consistent?!
+                    node.url = node.slug
+
+                    delete node.frontmatter
+                    delete node.fields
+
+                    return node
+                })
+                .reduce(transformer, [])
+
+    }
+})
+
+// Uncomment these for testing, to temporarily only do this for a small number of posts
+// module.exports = [ghostQueries[1]]
+// module.exports = [mdQueries[1]]
+module.exports = ghostQueries.concat(mdQueries)
