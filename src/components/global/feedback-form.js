976cfb88fdc3747c349801915f1ecf512a913942
Aileen Nowak <AileenCGN@gmail.com>
2018-10-17 18:31:42 +0700

    Send the correct feedback type

--- src/components/global/feedback-form.js
+++ src/components/global/feedback-form.js
@@ -41,8 +41,7 @@ class FeedbackForm extends React.Component {
                 "form-name": form.getAttribute(`name`),
                 "feedback-type": `Feedback`,
                 page: this.props.location.pathname,
-                email: this.state.email,
-                message: this.state.message,
+                ...this.state
             }),
         })
             .then(() => this.setState((state) => {
@@ -79,8 +78,6 @@ class FeedbackForm extends React.Component {
                         data-netlify-honeypot="bot-field"
                         onSubmit={this.handleSubmit}
                     >
-                        {/* The `form-name` hidden field is required to support form submissions without JavaScript
-                        <input type="hidden" name="form-name" value="feedback" /> */}
                         <p hidden>
                             <label>
                                 Don’t fill this out:{` `}
