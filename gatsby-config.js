1d83cbd95bc991e1fe0aec28ba8d2af5f90906a7
Aileen Nowak <AileenCGN@gmail.com>
2018-10-18 11:16:41 +0700

    Added code title plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,7 @@ module.exports = {
             resolve: `gatsby-transformer-remark`,
             options: {
                 plugins: [
+                    `gatsby-remark-code-titles`,
                     {
                         resolve: `gatsby-remark-prismjs`,
                         options: {
