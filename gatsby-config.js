c892c5e5bf34f840d6bd5865d75131714b980eb2
Aileen Nowak <AileenCGN@gmail.com>
2018-10-18 22:34:41 +0700

    Maybe image resizing?

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,10 +34,20 @@ module.exports = {
                 name: `markdown-pages`,
             },
         },
+        `gatsby-plugin-sharp`,
         {
             resolve: `gatsby-transformer-remark`,
             options: {
                 plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            // It's important to specify the maxWidth (in pixels) of
+                            // the content container as this plugin uses this as the
+                            // base for generating different widths of each image.
+                            maxWidth: 590,
+                        },
+                    },
                     `gatsby-remark-code-titles`,
                     `gatsby-remark-prismjs`, // TODO: make aliases work!
                     `gatsby-remark-external-links`,
