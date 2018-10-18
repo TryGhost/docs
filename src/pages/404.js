18a717dae2d87f2a7001d76a436fe93aebfbf350
Zimo <peter.zimon@gmail.com>
2018-10-18 20:27:46 +0200

    Somewhat better basic 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,20 @@
 import Layout from '../components/layouts/default'
 import React from 'react'
+import { Spirit } from '../components/spirit-styles'
+import Icon from '../components/global/icon'
+import { Link } from 'gatsby';
 
 const NotFoundPage = () => (
     <Layout headerDividerStyle="shadow">
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <div className={ Spirit.page.m + `pt-vw5 pb-vw5 flex flex-column items-center`}>
+            <Icon name="four-o-four-icon" className="w15 h-auto stroke-lightgrey" />
+            <h1 className={ Spirit.h1 }>404</h1>
+            <p className={ Spirit.p + `midgrey`}>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            <div className="flex mt5">
+                <Link to="/" className="pa2 dib blue hover-darkgrey link br b--whitegrey">Docs Home</Link>
+                <a href="https://ghost.org/" className="pa2 dib blue hover-darkgrey link">Ghost.org</a>
+            </div>
+        </div>
     </Layout>
 )
 
