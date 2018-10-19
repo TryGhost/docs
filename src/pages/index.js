7863c9910c44a94abcaae48cf94da6bd7b9322c7
Zimo <peter.zimon@gmail.com>
2018-10-19 16:50:35 +0200

    Fixing API box hover

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ import homeIllustration from '../images/home-illustration.svg' // eslint-disable
 class APIBox extends React.Component {
     render() {
         return (
-            <Link to={ this.props.to } className="flex items-start pa4 pa7-ns tdn bb b--whitegrey justify-between mih-10 flex-auto">
+            <Link to={ this.props.to } className="flex items-start pa4 pa7-ns tdn bb b--whitegrey justify-between mih-10 flex-auto api-box">
                 <span className="dib mr3 mt3 miw10 tc"><Icon name={ this.props.icon } className="stroke-middarkgrey-l2" /></span>
                 <div className="flex-auto">
                     <h4 className={ Spirit.h5 + `darkgrey mt2 mt0-l` }>{ this.props.title }</h4>
