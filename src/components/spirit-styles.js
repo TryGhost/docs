221c1d87e3a49ded9221980c49c21845a7ecc08f
Zimo <peter.zimon@gmail.com>
2018-10-24 15:21:13 +0200

    FAQ responsive refinements

--- src/components/spirit-styles.js
+++ src/components/spirit-styles.js
@@ -11,7 +11,7 @@ export const Spirit = {
     // Generic typography
     headline: ` f2 lh-h2 f-headline-l lh-h1-l fw6 ma0 measure--1-0 `,
     thinheadline: ` f2 lh-h2 f-headline-l lh-h1-l fw3 ma0 measure--1-0 `,
-    h1: ` f2 lh-h2 f1-l lh-h1-l fw3 ma0 pa0 measure--1-0 `,
+    h1: ` f1-mobile lh-h2 f1-l lh-h1-l fw3 ma0 pa0 measure--1-0 `,
     h2: ` f3 lh-h3 f2-l lh-h2-l fw5 ma0 pa0 `,
     h3: ` f4 lh-h4 f3-l lh-h3-l fw5 ma0 pa0 measure--0-2 `,
     h4: ` f5 lh-h5 f4-l lh-h4-l fw5 ma0 pa0 `,
