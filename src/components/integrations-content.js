b822d3660424ff9e62739ef35df9d5d477321073
Aileen Nowak <AileenCGN@gmail.com>
2018-11-01 18:31:04 +0700

    Unfuck header

--- src/components/integrations-content.js
+++ src/components/integrations-content.js
@@ -77,22 +77,27 @@ class IntegrationsContent extends React.Component {
 
         return (
             <>
-                <Layout title="Integrations" headerDividerStyle="shadow">
-                    <div className="bg-integrations-header-image">
-                        <div className="bg-integrations-header-cover">
-                            <header className="top-0 left-0 right-0 z-9999">
-                                <NavBar theme="light" />
-                            </header>
-                            <div className="pa-vw4 tc">
-                                <h1 className="ma0 pa0 f-headline white gh-integration-header-shadow">Ghost Integrations</h1>
-                                <p className="ma0 mt2 f4 white-80">All your favourite apps and tools, integrated with Ghost</p>
-                                <IntegrationSearchBox
-                                    searchActive={this.toggleSearch}
-                                    returnTo={this.props.location.pathname}
-                                />
+                <Layout
+                    title="Integrations"
+                    headerDividerStyle="shadow"
+                    header={
+                        <div className="bg-integrations-header-image">
+                            <div className="bg-integrations-header-cover">
+                                <header className="top-0 left-0 right-0 z-9999">
+                                    <NavBar theme="light" />
+                                </header>
+                                <div className="pa-vw4 tc">
+                                    <h1 className="ma0 pa0 f-headline white gh-integration-header-shadow">Ghost Integrations</h1>
+                                    <p className="ma0 mt2 f4 white-80">All your favourite apps and tools, integrated with Ghost</p>
+                                    <IntegrationSearchBox
+                                        searchActive={this.toggleSearch}
+                                        returnTo={this.props.location.pathname}
+                                    />
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    }
+                >
                     <IntegrationIndex>
                         <div className={Spirit.page.xl + `pt10`}>
                             <div className="flex br4">
