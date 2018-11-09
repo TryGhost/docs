60b6ee67f2e4948dd8a05cd7aeb54627d1d198bc
Aileen Nowak <AileenCGN@gmail.com>
2018-11-09 12:20:27 +0700

    🎨 Import cypress testing library so we can use it

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import 'cypress-testing-library/add-commands';
+
+Cypress.Commands.add('assertRoute', (route) => {
+    cy.url().should('equal', `${window.location.origin}${route}`);
+});
