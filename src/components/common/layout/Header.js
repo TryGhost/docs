94d2fa9abd5cddf04dd948b906a03856f3ec3f39
Aileen Nowak <AileenCGN@gmail.com>
2018-11-20 21:38:01 +0700

    ♻️  Refactor Header to stateless component and small improvements

--- src/components/common/layout/Header.js
+++ src/components/common/layout/Header.js
@@ -3,34 +3,20 @@ import PropTypes from 'prop-types'
 
 import NavBar from '../NavBar'
 
-class Header extends React.Component {
-    render() {
-        var headerShadow
-
-        switch (this.props.dividerStyle) {
-        case `hairline`:
-            headerShadow = `bb b--whitegrey`
-            break
-
-        case `shadow`:
-            headerShadow = `header-shadow`
-            break
-
-        case `none`:
-            headerShadow = ``
-            break
-        }
-
-        return (
-            <>
-                <header className={ headerShadow + ` fixed bg-white top-0 left-0 right-0 z-500 mb10` }>
-                    <NavBar theme="dark" />
-                </header>
-                <div className="h17"></div> {/* We need to push down all the content because of the fixed header */}
-            </>
-        )
-    }
+const headerShadow = {
+    hairline: `bb b--whitegrey`,
+    shadow: `header-shadow`,
+    none: ``,
 }
+
+const Header = ({ dividerStyle }) => (
+        <>
+            <header className={`${headerShadow[dividerStyle]} fixed bg-white top-0 left-0 right-0 z-500 mb10`}>
+                <NavBar theme="dark" />
+            </header>
+            <div className="h17"></div> {/* We need to push down all the content because of the fixed header */}
+        </>
+)
 
 Header.defaultProps = {
     dividerStyle: `hairline`,
