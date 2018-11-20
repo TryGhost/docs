4b282bd06bb0d09a1f620eaa044dd12cc2cf65c8
Aileen Nowak <AileenCGN@gmail.com>
2018-11-20 22:20:18 +0700

    ♻️  Refactor Layout to stateless component and small improvements

--- src/components/common/layout/Layout.js
+++ src/components/common/layout/Layout.js
@@ -2,44 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-// Partials and components
 import Header from './Header'
 import Footer from './Footer'
 import { SearchWrapper } from '../search'
 
-// Spirit
-// import 'ghost-spirit/public/spirit-brand.css'
-import '../../../styles/app.css'
-
 // Additional styles
+import '../../../styles/app.css'
 import '../../../styles/prism.css'
 
-class DefaultLayout extends React.Component {
-    render() {
-        const { children } = this.props
-        const header = this.props.header ? this.props.header : <Header dividerStyle={this.props.headerDividerStyle} />
+const DefaultLayout = ({ children, bodyClass, mainClass, header, headerDividerStyle }) => (
+        <>
+            <Helmet>
+                <html lang="en" className="fs-base" />
+                <link type="text/css" href="https://cloud.typography.com/6076934/6704592/css/fonts.css" rel="stylesheet" />
+                <body className={`${bodyClass} flex flex-column whitney f7 fw4 darkgrey readability`} />
+            </Helmet>
 
-        return (
-            <>
-                <Helmet>
-                    <html lang="en" className="fs-base" />
-                    <link type="text/css" href="https://cloud.typography.com/6076934/6704592/css/fonts.css" rel="stylesheet" />
-                    <body className={this.props.bodyClass + ` flex flex-column whitney f7 fw4 darkgrey readability`} />
-                </Helmet>
+            <SearchWrapper>
+                {header || <Header dividerStyle={headerDividerStyle} />}
 
-                <SearchWrapper>
-                    {header}
+                <main className={mainClass || `bg-whitegrey-l2 pb5 pb10-ns`}>
+                    {children}
+                </main>
 
-                    <main className={ this.props.mainClass ? this.props.mainClass : `bg-whitegrey-l2 pb5 pb10-ns` }>
-                        {children}
-                    </main>
-
-                    <Footer />
-                </SearchWrapper>
-            </>
-        )
-    }
-}
+                <Footer />
+            </SearchWrapper>
+        </>
+)
 
 DefaultLayout.defaultProps = {
     headerDividerStyle: `shadow`,
