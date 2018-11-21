d456db5f657c01881ce1da3d8d154d61507e8cd2
Aileen Nowak <AileenCGN@gmail.com>
2018-11-21 22:29:34 +0700

    ♻️  Refactor SidebarList and SidebarNav as stateless components

--- src/components/common/sidebar/SidebarList.js
+++ src/components/common/sidebar/SidebarList.js
@@ -3,32 +3,35 @@ import PropTypes from 'prop-types'
 
 import SidebarLink from './SidebarLink'
 
-class SidebarList extends React.Component {
-    render() {
-        const linkClasses = `midgrey fw4 hover-blue-l2`
-        const activeLinkClasses = `sidebarlink-active blue fw6`
+const SidebarList = ({ items, location }) => {
+    const linkClasses = `midgrey fw4 hover-blue-l2`
+    const activeLinkClasses = `sidebarlink-active blue fw6`
 
-        return (
-            <>
-                <ul className="relative sidebar-list ma0 pa0 list mb5 pl6 mt1">
-                    {this.props.items.map((item, j) => (
-                        <li key={j}>
-                            <SidebarLink
-                                link={item.link}
-                                title={item.title}
-                                linkClasses={(item.link === this.props.location.pathname ? activeLinkClasses : linkClasses)}
-                            />
-                        </li>
-                    ))}
-                </ul>
-            </>
-        )
-    }
+    return (
+        <ul className="relative sidebar-list ma0 pa0 list mb5 pl6 mt1">
+            {items.map((item, j) => (
+                <li key={j}>
+                    <SidebarLink
+                        link={item.link}
+                        title={item.title}
+                        linkClasses={(item.link === location.pathname ? activeLinkClasses : linkClasses)}
+                    />
+                </li>
+            ))}
+        </ul>
+    )
 }
 
 SidebarList.propTypes = {
-    items: PropTypes.array.isRequired,
-    location: PropTypes.object.isRequired,
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            link: PropTypes.string.isRequired,
+        }).isRequired,
+    ).isRequired,
+    location: PropTypes.shape({
+        pathname: PropTypes.string.isRequired,
+    }).isRequired,
 }
 
 export default SidebarList
--- src/components/common/sidebar/SidebarNav.js
+++ src/components/common/sidebar/SidebarNav.js
@@ -4,66 +4,56 @@ import PropTypes from 'prop-types'
 import SidebarLink from './SidebarLink'
 import SidebarList from './SidebarList'
 
-function groupExpanded(items, pathname) {
-    var breakException = {}
-
-    try {
-        items.forEach((item) => {
-            if (item.link === pathname) {
-                throw breakException
-            }
-        })
-    } catch (e) {
-        if (e !== breakException) {
+const SidebarNav = ({ sidebar, location }) => {
+    if (sidebar) {
+        try {
+            // declare as var here, so it's accessible outside of the try scope
+            var [sidebarfile] = require(`../../../data/sidebars/${sidebar}.yaml`)
+        } catch (e) {
             throw e
-        } else {
-            return true
         }
     }
-    return false
-}
 
-class SidebarNav extends React.Component {
-    render() {
-        const { sidebar, location } = this.props
-        const [sidebarfile] = sidebar ? require(`../../../data/sidebars/${sidebar}.yaml`) : []
-
-        if (!sidebarfile) {
-            return null
-        }
-
-        if (!sidebarfile.groups) {
-            return (
-                <div></div>
-            )
-        }
-
-        return (
-            <nav className="mt5 mb5 mt10-ns mb0-ns relative" data-cy="sidebar">
-                { sidebarfile.groups.map((group, i) => (
-                    <div key={ i } className="mt1">
-                        { groupExpanded(group.items, this.props.location.pathname) ?
-                            <>
-                                <h4 className="f5 fw5 link pa0 ma0">{ (group.items[0].link ? <SidebarLink link={ group.items[0].link } title={ group.group } linkClasses="midgrey link" /> : group.group) }</h4>
-                                <SidebarList
-                                    key={ i }
-                                    items={ group.items }
-                                    location={ location }
-                                /></> : <h4 className="f5 fw5 link pa0 ma0">{ (group.items[0].link ? <SidebarLink link={ group.items[0].link } title={ group.group } linkClasses="midgrey hover-blue-l2 link" /> : group.group) }</h4> }
-                    </div>
-                )) }
-            </nav>
-        )
+    if (!sidebar || !sidebarfile || !sidebarfile.groups) {
+        return null
     }
-}
 
-SidebarNav.propTypes = {
-    sidebar: PropTypes.string.isRequired,
-    location: PropTypes.object.isRequired,
+    return (
+        <nav className="mt5 mb5 mt10-ns mb0-ns relative" data-cy="sidebar">
+            {sidebarfile.groups.map((group, i) => (
+                <div key={i} className="mt1">
+                    {group.items.some(item => (item.link === location.pathname)) ?
+                        // Render a sidebar list with children, if any of the nested elements
+                        // matches our current location, so the group needs to
+                        <>
+                            <h4 className="f5 fw5 link pa0 ma0">
+                                {group.items[0].link ?
+                                    <SidebarLink link={group.items[0].link} title={group.group} linkClasses="midgrey link" />
+                                    : group.group
+                                }
+                            </h4>
+                            <SidebarList
+                                key={i}
+                                items={group.items}
+                                location={location}
+                            />
+                        </>
+                        : <h4 className="f5 fw5 link pa0 ma0">{(group.items[0].link ? <SidebarLink link={group.items[0].link} title={group.group} linkClasses="midgrey hover-blue-l2 link" /> : group.group)}</h4>}
+                </div>
+            ))}
+        </nav>
+    )
 }
 
 SidebarNav.defaultProps = {
     location: { pathname: `/` },
+}
+
+SidebarNav.propTypes = {
+    sidebar: PropTypes.string.isRequired,
+    location: PropTypes.shape({
+        pathname: PropTypes.string.isRequired,
+    }).isRequired,
 }
 
 export default SidebarNav
