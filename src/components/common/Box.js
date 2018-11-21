c54fd6ffecfe92871330859ef03f97c5844cf184
Aileen Nowak <AileenCGN@gmail.com>
2018-11-21 23:41:33 +0700

    ♻️  Convert Box into stateless component

--- src/components/common/Box.js
+++ src/components/common/Box.js
@@ -2,60 +2,51 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
-const getBoxClass = ({ elevation, onWhite, radius, to, href }) => {
-    // Classes for setting shadows
-    const shadowClasses = `shadow-${elevation} ${(onWhite ? `on-white` : ``)} ${(href || to ? `box-shadow-hover shadow-${elevation}-hover` : ``)} `
-
-    // Classes for setting border radius
+const Box = ({ children, to, href, className, elevation, radius, onWhite }) => {
+    const baseBoxClass = `bg-white`
+    // Shadow classes
+    const shadowClasses = `shadow-${elevation} ${(onWhite ? `on-white` : ``)} ${(href || to ? `box-shadow-hover shadow-${elevation}-hover` : ``)}`
+    // Border radius clss
     const radiusClasses = `br${radius}`
 
-    return shadowClasses + radiusClasses
-}
-class Box extends React.Component {
-    render() {
-        const children = this.props.children
-        const baseBoxClass = `bg-white`
-        const dynamicBoxClasses = getBoxClass(this.props)
-
-        if (this.props.to) {
-            return (
-                <Link to={this.props.to} className={`${baseBoxClass} ${dynamicBoxClasses} db ${this.props.className}`}>
-                    {children}
-                </Link>
-            )
-        } else if (this.props.href) {
-            return (
-                <a href={this.props.href} className={`${baseBoxClass} ${dynamicBoxClasses} db ${this.props.className}`} target="_blank" rel="noopener noreferrer">
-                    {children}
-                </a>
-            )
-        } else {
-            return (
-                <div className={`${baseBoxClass} ${dynamicBoxClasses} ${this.props.className}`}>
-                    {children}
-                </div>
-            )
-        }
+    if (to) {
+        // internal links
+        return (
+            <Link to={to} className={`${baseBoxClass} ${shadowClasses} ${radiusClasses} db ${className}`}>
+                {children}
+            </Link>
+        )
+    } else if (href) {
+        // external links
+        return (
+            <a href={href} className={`${baseBoxClass} ${shadowClasses} ${radiusClasses} db ${className}`} target="_blank" rel="noopener noreferrer">
+                {children}
+            </a>
+        )
+    } else {
+        // non-link boxes
+        return (
+            <div className={`${baseBoxClass} ${shadowClasses} ${radiusClasses} ${className}`}>
+                {children}
+            </div>
+        )
     }
 }
 
 Box.defaultProps = {
-    to: ``,
-    href: ``,
     elevation: `2`,
     radius: `3`,
     onWhite: false,
-    linkClassName: `link`,
 }
 
 Box.propTypes = {
+    children: PropTypes.node.isRequired,
     to: PropTypes.string,
     href: PropTypes.string,
     elevation: PropTypes.string,
     radius: PropTypes.string,
     onWhite: PropTypes.bool,
-    className: PropTypes.any,
-    children: PropTypes.node.isRequired,
+    className: PropTypes.string,
 }
 
 export default Box
