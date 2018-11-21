bbd61d3e4f1efcff90a2d26b7de99e2e3a5ac529
Aileen Nowak <AileenCGN@gmail.com>
2018-11-21 20:03:10 +0700

    ♻️  Turn SidebarLink into stateless component

--- src/components/common/sidebar/SidebarLink.js
+++ src/components/common/sidebar/SidebarLink.js
@@ -2,23 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
-class SidebarLink extends React.Component {
-    render() {
-        if (this.props.link) {
-            if (this.props.link.match(/^\s?http(s?)/gi)) {
-                return (
-                    <a href={ this.props.link } className={ `link db pv6px pr2 lh-1-5 pl0 ` + this.props.linkClasses } target="_blank" rel="noopener noreferrer">{ this.props.title }</a>
-                )
-            } else {
-                return (
-                    <Link to={ this.props.link } className={ `link db pv6px pr2 lh-1-5 pl0 ${this.props.linkClasses}` }>{ this.props.title }</Link>
-                )
-            }
+const SidebarLink = ({ link, title, linkClasses }) => {
+    if (link) {
+        if (link.match(/^\s?http(s?)/gi)) {
+            // use anchor links for external links
+            return <a href={link} className={`link db pv6px pr2 lh-1-5 pl0 ${linkClasses}`} target="_blank" rel="noopener noreferrer">{title}</a>
         } else {
-            return (
-                <>{ this.props.title }</>
-            )
+            return <Link to={link} className={`link db pv6px pr2 lh-1-5 pl0 ${linkClasses}`}>{title}</Link>
         }
+    } else {
+        return (
+            <>{this.props.title}</>
+        )
     }
 }
 
