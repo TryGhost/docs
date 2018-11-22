4e0465c061a721ef1f38e6d1d903f893c4dffab9
Aileen Nowak <AileenCGN@gmail.com>
2018-11-22 15:01:35 +0700

    🚚 Outsource MainBox component from HomeHeader and rename it to HomeHeaderBox

--- src/components/home/HomeHeader.js
+++ src/components/home/HomeHeader.js
@@ -1,34 +1,9 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
 
 import { SearchModal } from '../common/search'
-import { Icon, NavBar } from '../common'
+import { NavBar } from '../common'
 import { Spirit } from '../../styles/spirit-styles'
-
-const MainBox = ({ to, title, color, icon, children }) => (
-    <Link className="col-12 col-4-ns pa5 pa8-ns pa10-l pt8-l flex flex-column-ns items-start tdn content-stretch home-main-box-shadow db br4 bg-white" to={ to }>
-        <Icon name={icon} className={`w10 h-auto w12-ns stroke-w--1-5 mr3 mb2 stroke-${color}`} />
-        <div className="flex flex-column justify-between flex-auto">
-            <div>
-                <h2 className={`${Spirit.h4} mt0 mt2-ns darkgrey flex-shrink-1`}>{title}</h2>
-                <p className={`${Spirit.small} midgrey`}>{children}</p>
-            </div>
-            <span className={ `${color} dib mt2 link fw5 f7 f5-ns flex items-center`}>
-                Learn more
-                <Icon name="arrow-right" className={`w3 h3 ml2 fill-${color}`} />
-            </span>
-        </div>
-    </Link>
-)
-
-MainBox.propTypes = {
-    to: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    color: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired,
-}
+import HomeHeaderBox from './HomeHeaderBox'
 
 // Custom headings must be React components. You should include the <NavBar /> component
 // somewhere in it. You can optionally set the theme of the navbar to `dark` or `light`.
@@ -42,32 +17,32 @@ const HomeHeader = () => (
             <SearchModal isHome />
 
             <section className="grid-12 gutter-row-20 gutter-36-ns mt10 mt20-ns mt25-l miw-100 miw-auto-ns home-main-box-margin-ns z-999">
-                <MainBox
+                <HomeHeaderBox
                     to="/concepts/introduction/"
                     title="Core Concepts"
                     icon="blocks"
                     color="purple"
                 >
                     Understand the fundamentals of Ghost development.
-                </MainBox>
+                </HomeHeaderBox>
 
-                <MainBox
+                <HomeHeaderBox
                     to="/setup/"
                     title="Setup Guide"
                     icon="rocket"
                     color="blue"
                 >
                     Setting up a Ghost site on a server or locally.
-                </MainBox>
+                </HomeHeaderBox>
 
-                <MainBox
+                <HomeHeaderBox
                     to="/tutorials/"
                     title="Tutorials"
                     icon="typing"
                     color="tutorial-green"
                 >
                     Browse tutorials for most common setup and development use-cases.
-                </MainBox>
+                </HomeHeaderBox>
             </section>
         </div>
     </div>
