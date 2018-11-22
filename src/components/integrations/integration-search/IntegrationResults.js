6a64e6d900609567b8d4db0401d2a974ee86ded1
Aileen Nowak <AileenCGN@gmail.com>
2018-11-22 16:27:14 +0700

    ♻️  Refactor IntegrationResults as stateless component

--- src/components/integrations/integration-search/IntegrationResults.js
+++ src/components/integrations/integration-search/IntegrationResults.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connectHits } from 'react-instantsearch-dom'
-import IntegrationBox from '../IntegrationBox'
 
-class IntegrationResults extends React.Component {
-    render() {
-        return (
-            <div className="gh-integrations w-100">
-                {this.props.hits.map(hit => <IntegrationBox key={hit.objectID} hit={hit} />)}
-            </div>
-        )
-    }
-}
+import { IntegrationBox } from '../.'
+
+const IntegrationResults = ({ hits }) => (
+    <div className="gh-integrations w-100">
+        {hits.map(hit => <IntegrationBox key={hit.objectID} hit={hit} />)}
+    </div>
+)
 
 IntegrationResults.propTypes = {
-    hits: PropTypes.array.isRequired,
+    hits: PropTypes.arrayOf(
+        PropTypes.shape({
+            objectID: PropTypes.string.isRequired,
+        }).isRequired,
+    ).isRequired,
 }
 
 const IntegrationResultsList = connectHits(IntegrationResults)
