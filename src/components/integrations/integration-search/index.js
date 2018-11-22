3dc6b00e76191fca7044a8b65d95968dd8a92792
Aileen Nowak <AileenCGN@gmail.com>
2018-11-22 15:49:20 +0700

    🔥 Removed unused IntegrationsFilterMenu component

--- src/components/integrations/integration-search/index.js
+++ src/components/integrations/integration-search/index.js
@@ -1,4 +1,3 @@
 export { default as IntegrationResults } from './IntegrationResults'
-export { default as IntegrationFilterMenu } from './IntegrationFilterMenu'
 export { default as IntegrationSearchBox } from './IntegrationSearchBox'
 export { default as IntegrationIndex } from './IntegrationIndex'
