3f8cfc1edc0dffd665168b16814433a5ffdea068
Aileen Nowak <AileenCGN@gmail.com>
2018-11-22 16:39:46 +0700

    ♻️ Refactor ArticleMetaGhost into stateless component

--- src/components/common/meta/ArticleMetaGhost.js
+++ src/components/common/meta/ArticleMetaGhost.js
@@ -7,105 +7,102 @@ import getPostExcerpt from '../../../utils/getPostExcerpt'
 import getAuthorProperties from './getAuthorProperties'
 import ImageMeta from './ImageMeta'
 
-class ArticleMetaGhost extends React.Component {
-    render() {
-        const { ghostPost } = this.props.data
-        const { canonical, fetchAuthorData, title, overwriteDefaultImage, image } = this.props
-        const { siteMetadata } = this.props.data.site
+const ArticleMetaGhost = ({ data, canonical, fetchAuthorData, title, overwriteDefaultImage, image }) => {
+    const { ghostPost } = data
+    const { siteMetadata } = data.site
 
-        const excerpt = getPostExcerpt(ghostPost)
-        const author = getAuthorProperties(ghostPost.primary_author, fetchAuthorData)
-        const publicTags = _.map(_.filter(ghostPost.tags, { visibility: `public` }), `name`)
-        const primaryTag = _.get(ghostPost.primaryTag, `name`, publicTags[0])
-        const seoImage = (overwriteDefaultImage && ghostPost.feature_image) ? ghostPost.feature_image : image
+    const excerpt = getPostExcerpt(ghostPost)
+    const author = getAuthorProperties(ghostPost.primary_author, fetchAuthorData)
+    const publicTags = _.map(_.filter(ghostPost.tags, { visibility: `public` }), `name`)
+    const primaryTag = _.get(ghostPost.primaryTag, `name`, publicTags[0])
+    const seoImage = (overwriteDefaultImage && ghostPost.feature_image) ? ghostPost.feature_image : image
 
-        return (
-            <>
-                <Helmet>
-                    <title>{ghostPost.meta_title || title || ghostPost.title}</title>
-                    <meta name="description" content={ghostPost.meta_description || excerpt} />
-                    <link rel="canonical" href={canonical} />
+    return (
+        <>
+            <Helmet>
+                <title>{ghostPost.meta_title || title || ghostPost.title}</title>
+                <meta name="description" content={ghostPost.meta_description || excerpt} />
+                <link rel="canonical" href={canonical} />
 
-                    <meta property="og:site_name" content={siteMetadata.title} />
-                    <meta name="og:type" content="article" />
-                    <meta name="og:title"
-                        content={
-                            ghostPost.og_title ||
-                            title ||
-                            ghostPost.meta_title ||
-                            ghostPost.title
-                        }
-                    />
-                    <meta name="og:description"
-                        content={
-                            ghostPost.og_description ||
-                            excerpt ||
-                            ghostPost.meta_description
-                        }
-                    />
-                    <meta property="og:url" content={canonical} />
-                    <meta property="article:published_time" content={ghostPost.published_at} />
-                    <meta property="article:modified_time" content={ghostPost.updated_at} />
-                    {publicTags.map((keyword, i) => (<meta property="article:tag" content={keyword} key={i} />))}
-                    <meta property="article:author" content="https://www.facebook.com/ghost/" />
+                <meta property="og:site_name" content={siteMetadata.title} />
+                <meta name="og:type" content="article" />
+                <meta name="og:title"
+                    content={
+                        ghostPost.og_title ||
+                        title ||
+                        ghostPost.meta_title ||
+                        ghostPost.title
+                    }
+                />
+                <meta name="og:description"
+                    content={
+                        ghostPost.og_description ||
+                        excerpt ||
+                        ghostPost.meta_description
+                    }
+                />
+                <meta property="og:url" content={canonical} />
+                <meta property="article:published_time" content={ghostPost.published_at} />
+                <meta property="article:modified_time" content={ghostPost.updated_at} />
+                {publicTags.map((keyword, i) => (<meta property="article:tag" content={keyword} key={i} />))}
+                <meta property="article:author" content="https://www.facebook.com/ghost/" />
 
-                    <meta name="twitter:title"
-                        content={
-                            ghostPost.twitter_title ||
-                            title ||
-                            ghostPost.meta_title ||
-                            ghostPost.title
+                <meta name="twitter:title"
+                    content={
+                        ghostPost.twitter_title ||
+                        title ||
+                        ghostPost.meta_title ||
+                        ghostPost.title
+                    }
+                />
+                <meta name="twitter:description"
+                    content={
+                        ghostPost.twitter_description ||
+                        excerpt ||
+                        ghostPost.meta_description
+                    }
+                />
+                <meta name="twitter:url" content={canonical} />
+                {/* <meta name="twitter.label1" content="Reading time" /> */}
+                {/* <meta name="twitter:data1" content="TODO: Reading time helper and replace existing `label1` data" /> */}
+                <meta name="twitter:label1" content="Written by" />
+                <meta name="twitter:data1" content={author.name} />
+                {primaryTag ? <meta name="twitter:label2" content="Filed under" /> : null}
+                {primaryTag ? <meta name="twitter:data2" content={primaryTag} /> : null}
+                <meta name="twitter:site" content="@tryghost" />
+                <meta name="twitter:creator" content="@tryghost" />
+                <script type="application/ld+json">{`
+                    {
+                        "@context": "https://schema.org/",
+                        "@type": "Article",
+                        "author": {
+                            "@type": "Person",
+                            "name": "${author.name}",
+                            ${author.image ? author.sameAsArray ? `"image": "${author.image}",` : `"image": "${author.image}"` : ``}
+                            ${author.sameAsArray ? `"sameAs": ${author.sameAsArray}` : ``}
+                        },
+                        ${publicTags.length ? `"keywords": "${_.join(publicTags, `, `)}",` : ``}
+                        "headline": "${ghostPost.meta_title || title || ghostPost.title}",
+                        "url": "${canonical}",
+                        "datePublished": "${ghostPost.published_at}",
+                        "dateModified": "${ghostPost.updated_at}",
+                        "image": {
+                            "@type": "ImageObject",
+                            "url": "${seoImage}",
+                            "width": 1000,
+                            "height": 563
+                        },
+                        "description": "${ghostPost.meta_description || excerpt}",
+                        "mainEntityOfPage": {
+                            "@type": "WebPage",
+                            "@id": "${siteMetadata.siteUrl}"
                         }
-                    />
-                    <meta name="twitter:description"
-                        content={
-                            ghostPost.twitter_description ||
-                            excerpt ||
-                            ghostPost.meta_description
-                        }
-                    />
-                    <meta name="twitter:url" content={canonical} />
-                    {/* <meta name="twitter.label1" content="Reading time" /> */}
-                    {/* <meta name="twitter:data1" content="TODO: Reading time helper and replace existing `label1` data" /> */}
-                    <meta name="twitter:label1" content="Written by" />
-                    <meta name="twitter:data1" content={author.name} />
-                    {primaryTag ? <meta name="twitter:label2" content="Filed under" /> : null}
-                    {primaryTag ? <meta name="twitter:data2" content={primaryTag} /> : null}
-                    <meta name="twitter:site" content="@tryghost" />
-                    <meta name="twitter:creator" content="@tryghost" />
-                    <script type="application/ld+json">{`
-                        {
-                            "@context": "https://schema.org/",
-                            "@type": "Article",
-                            "author": {
-                                "@type": "Person",
-                                "name": "${author.name}",
-                                ${author.image ? author.sameAsArray ? `"image": "${author.image}",` : `"image": "${author.image}"` : ``}
-                                ${author.sameAsArray ? `"sameAs": ${author.sameAsArray}` : ``}
-                            },
-                            ${publicTags.length ? `"keywords": "${_.join(publicTags, `, `)}",` : ``}
-                            "headline": "${ghostPost.meta_title || title || ghostPost.title}",
-                            "url": "${canonical}",
-                            "datePublished": "${ghostPost.published_at}",
-                            "dateModified": "${ghostPost.updated_at}",
-                            "image": {
-                                "@type": "ImageObject",
-                                "url": "${seoImage}",
-                                "width": 1000,
-                                "height": 563
-                            },
-                            "description": "${ghostPost.meta_description || excerpt}",
-                            "mainEntityOfPage": {
-                                "@type": "WebPage",
-                                "@id": "${siteMetadata.siteUrl}"
-                            }
-                        }
-                    `}</script>
-                </Helmet>
-                <ImageMeta image={seoImage} />
-            </>
-        )
-    }
+                    }
+                `}</script>
+            </Helmet>
+            <ImageMeta image={seoImage} />
+        </>
+    )
 }
 
 // "publisher": {
