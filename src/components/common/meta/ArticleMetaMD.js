6c7f0bc2b5356110a6d3e07e75b13ab005336503
Aileen Nowak <AileenCGN@gmail.com>
2018-11-22 16:43:51 +0700

    ♻️ Refactor ArticleMetaMD as stateless component

--- src/components/common/meta/ArticleMetaMD.js
+++ src/components/common/meta/ArticleMetaMD.js
@@ -6,79 +6,76 @@ import _ from 'lodash'
 import ImageMeta from './ImageMeta'
 import { getMetaImageUrls } from '.'
 
-class ArticleMetaMD extends React.Component {
-    render() {
-        const post = this.props.data.markdownRemark
-        const fm = post.frontmatter
+const ArticleMetaMD = ({ data, canonical }) => {
+    const post = data.markdownRemark
+    const fm = post.frontmatter
+    const { siteMetadata } = data.site
 
-        // Convert the frontmatter date into ISO String but, and use a fixed
-        // date, if no date is set. The published date should not change once set.
-        const publishedAtISODate = fm.date ? new Date(fm.date).toISOString() : new Date(`2018-10-15`).toISOString()
-        const { canonical } = this.props
-        const { siteMetadata } = this.props.data.site
-        const primaryTag = fm.keywords && fm.keywords.length ? fm.keywords[0] : null
-        const seoImage = getMetaImageUrls()
+    // Convert the frontmatter date into ISO String but, and use a fixed
+    // date, if no date is set. The published date should not change once set.
+    const publishedAtISODate = fm.date ? new Date(fm.date).toISOString() : new Date(`2018-10-15`).toISOString()
+    const primaryTag = fm.keywords && fm.keywords.length ? fm.keywords[0] : null
+    const seoImage = getMetaImageUrls()
 
-        return (
-            <>
-                <Helmet>
-                    <title>{fm.meta_title || fm.title}</title>
-                    <meta name="description" content={fm.meta_description || post.excerpt} />
-                    <link rel="canonical" href={canonical} />
+    return (
+        <>
+            <Helmet>
+                <title>{fm.meta_title || fm.title}</title>
+                <meta name="description" content={fm.meta_description || post.excerpt} />
+                <link rel="canonical" href={canonical} />
 
-                    <meta property="og:site_name" content={siteMetadata.title} />
-                    <meta name="og:type" content="article" />
-                    <meta name="og:title" content={fm.meta_title || fm.title} />
-                    <meta name="og:description" content={fm.meta_description || post.excerpt} />
-                    <meta property="og:url" content={canonical} />
-                    <meta property="article:published_time" content={publishedAtISODate} />
-                    {fm.keywords.map((keyword, i) => (<meta property="article:tag" content={keyword} key={i} />))}
-                    <meta property="article:author" content="https://www.facebook.com/ghost/" />
+                <meta property="og:site_name" content={siteMetadata.title} />
+                <meta name="og:type" content="article" />
+                <meta name="og:title" content={fm.meta_title || fm.title} />
+                <meta name="og:description" content={fm.meta_description || post.excerpt} />
+                <meta property="og:url" content={canonical} />
+                <meta property="article:published_time" content={publishedAtISODate} />
+                {fm.keywords.map((keyword, i) => (<meta property="article:tag" content={keyword} key={i} />))}
+                <meta property="article:author" content="https://www.facebook.com/ghost/" />
 
-                    <meta name="twitter:title" content={fm.meta_title || fm.title} />
-                    <meta name="twitter:description" content={fm.meta_description || post.excerpt} />
-                    <meta name="twitter:url" content={canonical} />
-                    <meta name="twitter.label1" content="Reading time" />
-                    <meta name="twitter:data1" content={`${post.timeToRead} min read`} />
-                    {primaryTag ? <meta name="twitter:label2" content="Filed under" /> : null}
-                    {primaryTag ? <meta name="twitter:data2" content={primaryTag} /> : null}
-                    <meta name="twitter:site" content="@tryghost" />
-                    <meta name="twitter:creator" content="@tryghost" />
-                    <script type="application/ld+json">{`
-                        {
-                            "@context": "https://schema.org/",
-                            "@type": "Article",
-                            "author": {
-                                "@type": "Person",
-                                "name": "Ghost",
-                                "sameAs": [
-                                    "https://ghost.org/",
-                                    "https://www.facebook.com/ghost/",
-                                    "https://twitter.com/tryghost/"
-                                ]
-                            },
-                            ${fm.keywords.length ? `"keywords": "${_.join(fm.keywords, `, `)}",` : ``}
-                            "headline": "${fm.meta_title || fm.title}",
-                            "url": "${canonical}",
-                            "datePublished": "${publishedAtISODate}",
-                            "image": {
-                                "@type": "ImageObject",
-                                "url": "${seoImage}",
-                                "width": 1000,
-                                "height": 563
-                            },
-                            "description": "${fm.meta_description || post.excerpt}",
-                            "mainEntityOfPage": {
-                                "@type": "WebPage",
-                                "@id": "${siteMetadata.siteUrl}"
-                            }
+                <meta name="twitter:title" content={fm.meta_title || fm.title} />
+                <meta name="twitter:description" content={fm.meta_description || post.excerpt} />
+                <meta name="twitter:url" content={canonical} />
+                <meta name="twitter.label1" content="Reading time" />
+                <meta name="twitter:data1" content={`${post.timeToRead} min read`} />
+                {primaryTag ? <meta name="twitter:label2" content="Filed under" /> : null}
+                {primaryTag ? <meta name="twitter:data2" content={primaryTag} /> : null}
+                <meta name="twitter:site" content="@tryghost" />
+                <meta name="twitter:creator" content="@tryghost" />
+                <script type="application/ld+json">{`
+                    {
+                        "@context": "https://schema.org/",
+                        "@type": "Article",
+                        "author": {
+                            "@type": "Person",
+                            "name": "Ghost",
+                            "sameAs": [
+                                "https://ghost.org/",
+                                "https://www.facebook.com/ghost/",
+                                "https://twitter.com/tryghost/"
+                            ]
+                        },
+                        ${fm.keywords.length ? `"keywords": "${_.join(fm.keywords, `, `)}",` : ``}
+                        "headline": "${fm.meta_title || fm.title}",
+                        "url": "${canonical}",
+                        "datePublished": "${publishedAtISODate}",
+                        "image": {
+                            "@type": "ImageObject",
+                            "url": "${seoImage}",
+                            "width": 1000,
+                            "height": 563
+                        },
+                        "description": "${fm.meta_description || post.excerpt}",
+                        "mainEntityOfPage": {
+                            "@type": "WebPage",
+                            "@id": "${siteMetadata.siteUrl}"
                         }
-                    `}</script>
-                </Helmet>
-                <ImageMeta image={seoImage} />
-            </>
-        )
-    }
+                    }
+                `}</script>
+            </Helmet>
+            <ImageMeta image={seoImage} />
+        </>
+    )
 }
 
 // "publisher": {
