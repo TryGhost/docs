b84d3fa5083a536cae352e8fbe0efbaa0eb855ce
Aileen Nowak <AileenCGN@gmail.com>
2018-11-23 16:00:22 +0700

    🚚 Outsourced HomeAPIBox to home components

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,1 +1,2 @@
 export { default as HomeHeader } from './HomeHeader'
+export { default as HomeAPIBox } from './HomeAPIBox'
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,26 +5,9 @@ import PropTypes from 'prop-types'
 
 import { Icon, Box } from '../components/common'
 import { Layout } from '../components/common/layout'
-import { HomeHeader } from '../components/home'
+import { HomeHeader, HomeAPIBox } from '../components/home'
 import { Spirit } from '../styles/spirit-styles'
 import { MetaData, getMetaImageUrls } from '../components/common/meta'
-
-const HomeAPIBox = ({ to, icon, title, children }) => (
-    <Link to={to} className="flex items-start pa4 pa7-ns tdn bb b--whitegrey justify-between mih-10 flex-auto api-box">
-        <span className="dib mr3 mt3 miw10 tc"><Icon name={icon} className="stroke-middarkgrey-l2" /></span>
-        <div className="flex-auto">
-            <h4 className={`${Spirit.h5} darkgrey mt2 mt0-l`}>{title}</h4>
-            <p className={`${Spirit.small} midgrey`}>{children}</p>
-        </div>
-    </Link>
-)
-
-HomeAPIBox.propTypes = {
-    to: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired,
-}
 
 const HomeFAQLink = ({ to, title, children }) => (
     <Link to={to} className="midgrey home-faq-question link dib pt1 pb5">
