a672d11d2b011a2d6192b047cd2b404c6d51268e
Hannah Wolfe <erisds@gmail.com>
2018-12-04 17:16:01 +0000

    Use browser safe JS

--- gatsby/onRouteUpdate.js
+++ gatsby/onRouteUpdate.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /**
  * Trust All Scripts
  *
@@ -6,21 +7,24 @@
  *
  * This works for any script that then injects content into the page
  * via ids/classnames etc.
+ *
+ * TODO: use our browser eslint for this code
  */
-const trustAllScripts = () => {
-    const scriptNodes = document.querySelectorAll(`.external-scripts script`)
+var trustAllScripts = function () {
+    var scriptNodes = document.querySelectorAll('.external-scripts script');
 
-    scriptNodes.forEach((node) => {
+    for (var i = 0; i < scriptNodes.length; i += 1) {
+        var node = scriptNodes[i];
         // @TODO do the same for inline scripts?
         if (node.attributes.src) {
-            let s = document.createElement(`script`)
-            s.type = `text/javascript`
-            s.src = node.attributes.src.value
-            document.getElementsByTagName(`head`)[0].appendChild(s)
+            var s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.src = node.attributes.src.value;
+            document.getElementsByTagName('head')[0].appendChild(s);
         }
-    })
-}
+    }
+};
 
 module.exports = function () {
-    trustAllScripts()
-}
+    trustAllScripts();
+};
