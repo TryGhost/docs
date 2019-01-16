5462477170419d8f53335ee3adfbe54a06958283
Aileen Nowak <AileenCGN@gmail.com>
2019-01-16 13:47:38 +0700

    ♻️ Refactor tag-utils to use tags helper

--- src/utils/tag-utils.js
+++ src/utils/tag-utils.js
@@ -1,22 +1,6 @@
 import PropTypes from 'prop-types'
 import _ from 'lodash'
-
-/* removeInternalTags
-* Takes an array of tags (Ghost post) and return a filtered array
-* without internal tags, starting with a `#`
-*/
-export const removeInternalTags = function removeInternalTags(tags) {
-    // Get rid of internal tags
-    return tags.filter(tag => !tag.name.match(/^#/))
-}
-
-removeInternalTags.proptypes = {
-    tags: PropTypes.arrayOf(
-        PropTypes.shape({
-            name: PropTypes.string.isRequired,
-        }).isRequired,
-    ).isRequired,
-}
+import { tags as tagsHelper } from '@tryghost/helpers'
 
 /* getTagsforPostCollection
 * Takes a Ghost post object and a link prefix and returns the used tags
@@ -24,21 +8,23 @@ removeInternalTags.proptypes = {
 * duplicates, and are sorted ascending by name. The tags array can be used to
 * programmatically generate a tags cloud or menu.
 */
-// TODO: rename linkPrefix to permalink and use with `:slug`?
 export const getTagsforPostCollection = function getTagsforPostCollection(posts, linkPrefix) {
-    const tags = []
+    let tags = []
     // remove any added `/`, as we add them later again
     linkPrefix = /^(?:\/?)([a-zA-Z\d-]*)(?:\/?)/i.exec(linkPrefix)[1]
 
     _.forEach(posts, ({ node }) => {
-        _.map(node.tags, (tag) => {
-            tag.link = linkPrefix ? `/${linkPrefix}/${tag.slug}/` : `/${tag.slug}/`
-        })
-
-        tags.push(node.tags)
+        const publicTags = tagsHelper(node, { visibility: `public`, fn: tag => tag })
+        tags.push(publicTags)
     })
 
-    return _.sortedUniqBy(removeInternalTags(_.sortBy(_.flattenDeep(tags), `name`)), `name`)
+    tags = _.flattenDeep(tags)
+
+    _.map(tags, (tag) => {
+        tag.link = linkPrefix ? `/${linkPrefix}/${tag.slug}/` : `/${tag.slug}/`
+    })
+
+    return _.sortedUniqBy(_.sortBy(_.flattenDeep(tags), `name`), `name`)
 }
 
 getTagsforPostCollection.proptypes = {
