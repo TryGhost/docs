c2f40dee82a9cb105abe299df9c5745623755eae
Aileen Nowak <AileenCGN@gmail.com>
2019-02-08 17:44:27 +0700

    🚚 Destructure gatsby-browser file to show fn names
    refs #70
    
    - Restructure `gatsby-browser.js` file to return an object containing the method names that we're using for each browser API endpoint so it's more clear to understand what each method does.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,14 @@
-exports.onRouteUpdate = require(`./gatsby/onRouteUpdate`)
-exports.onPreRouteUpdate = require(`./gatsby/onPreRouteUpdate`)
+const onRouteUpdate = require(`./gatsby/onRouteUpdate`)
+const onPreRouteUpdate = require(`./gatsby/onPreRouteUpdate`)
+
+exports.onRouteUpdate = () => {
+    return {
+        trustAllScripts: onRouteUpdate(),
+    }
+}
+
+exports.onPreRouteUpdate = () => {
+    return {
+        killServiceWorker: onPreRouteUpdate(),
+    }
+}
