ead91cec71ab46e92a0aa93ec90a8e65f02d8812
Aileen Nowak <AileenCGN@gmail.com>
2019-02-14 15:04:59 +0700

    🎨 Wrap returned Object in Promise.props
    refs #70
    
    - Simply returning the functions for the API methods as a plain Object might cause async issue, as the receiving Gatsby Node API isn't aware of the fact that some of those functions return Promises
    - To keep the object pattern, but make sure Gatsby knows it's dealing with a Promises, I wrapped the returned object in a `Promise.props()` which is fulfilled only when all containing Promises are as well.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,13 @@
+const Promise = require(`bluebird`)
 const createPages = require(`./gatsby/createPages`)
 const onCreateNode = require(`./gatsby/onCreateNode`)
 
-exports.createPages = ({ graphql, actions }) => {
-    return {
-        createRedirects: createPages.createRedirects({ actions }),
-        createGhostPages: createPages.createGhostPages({ graphql, actions }),
-        createMarkdownPages: createPages.createMarkdownPages({ graphql, actions }),
-    }
-}
+exports.createPages = ({ graphql, actions }) => Promise.props({
+    createRedirects: createPages.createRedirects({ actions }),
+    createGhostPages: createPages.createGhostPages({ graphql, actions }),
+    createMarkdownPages: createPages.createMarkdownPages({ graphql, actions }),
+})
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-    return {
-        createMarkdownNodeFields: onCreateNode.createMarkdownNodeFields(({ node, getNode, actions })),
-    }
-}
+exports.onCreateNode = ({ node, getNode, actions }) => Promise.props({
+    createMarkdownNodeFields: onCreateNode.createMarkdownNodeFields(({ node, getNode, actions })),
+})
