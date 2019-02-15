0883b12c3c00d6d2a99856d90cd3eb94dcc4e8f3
Aileen Nowak <AileenCGN@gmail.com>
2019-02-15 14:33:23 +0700

    🎨 Switch to Promise.all() and async/await for gatsby-node
    refs #70
    
    - Use Promise.all() where we have multiple fn to wait for and they could potentially block each other
    - Use async/await when it's only one and no timing issue is given

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,10 @@
-const Promise = require(`bluebird`)
 const createPages = require(`./gatsby/createPages`)
 const onCreateNode = require(`./gatsby/onCreateNode`)
 
-exports.createPages = ({ graphql, actions }) => Promise.props({
-    createRedirects: createPages.createRedirects({ actions }),
-    createGhostPages: createPages.createGhostPages({ graphql, actions }),
-    createMarkdownPages: createPages.createMarkdownPages({ graphql, actions }),
-})
+exports.createPages = ({ graphql, actions }) => Promise.all([
+    createPages.createRedirects({ actions }),
+    createPages.createGhostPages({ graphql, actions }),
+    createPages.createMarkdownPages({ graphql, actions }),
+])
 
-exports.onCreateNode = ({ node, getNode, actions }) => Promise.props({
-    createMarkdownNodeFields: onCreateNode.createMarkdownNodeFields(({ node, getNode, actions })),
-})
+exports.onCreateNode = async ({ node, getNode, actions }) => await onCreateNode.createMarkdownNodeFields(({ node, getNode, actions }))
