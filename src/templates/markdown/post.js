094fba501393c9a25f22b8588451a73378c4a663
Aileen Nowak <aileen@ghost.org>
2019-02-18 15:10:26 +0700

    ✨ Edit on GitHub link for Markdown files (#86)

--- src/templates/markdown/post.js
+++ src/templates/markdown/post.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
+import url from 'url'
 
 import { Layout } from '../../components/common/layout'
 import { Spirit } from '../../styles/spirit-styles'
@@ -8,6 +9,38 @@ import { SidebarNav } from '../../components/common/sidebar'
 import { PrevNextSection } from '../../components/common/prev-next'
 import { PostHeader, FeedbackForm, Icon, TOC } from '../../components/common'
 import { MetaData, getMetaImageUrls } from '../../components/common/meta'
+
+const getGitHubLink = (absoluteFilePath) => {
+    let contentFilePath = ``
+
+    // testing only for `/content` doesn't work as we have a URL that contains
+    // `/content` twice. Therefore we're testing for each API version docs and
+    // the local content files.
+    const gitHubRepos = [
+        {
+            regex: /^(?:\S*\/content\/api\/v0\.11\/)(\S*)/i,
+            url: `https://github.com/TryGhost/docs-api/blob/v0.11/`,
+        },
+        {
+            regex: /^(?:\S*\/content\/api\/v2\/)(\S*)/i,
+            url: `https://github.com/TryGhost/docs-api/blob/master/`,
+        },
+        {
+            regex: /^(?:\S*\/content\/)(\S*)/i,
+            url: `https://github.com/TryGhost/docs/blob/master/content/`,
+        },
+    ]
+
+    // Using for...of here as we need to stop iterating as soon as we have a match
+    for (let repo of gitHubRepos) {
+        if (repo.regex.test(absoluteFilePath)) {
+            contentFilePath = url.resolve(repo.url, repo.regex.exec(absoluteFilePath)[1])
+            break
+        }
+    }
+
+    return contentFilePath
+}
 
 class Post extends React.Component {
     constructor(props) {
@@ -29,6 +62,9 @@ class Post extends React.Component {
     render() {
         const { location } = this.props
         const post = this.props.data.markdownRemark
+
+        const githubLink = getGitHubLink(post.fileAbsolutePath)
+
         const imageUrl = getMetaImageUrls()
         const sideBarLayout = {}
 
@@ -81,8 +117,20 @@ class Post extends React.Component {
                         }
                         <div>
                             <div className={`w-100 mw-content bg-white shadow-2 br4 ${(this.state.isToggleOn ? `` : ` br--bottom`)}`}>
-                                <article className="flex-auto pa5 pa8-m pa15-l pt10-ns pb10-ns pt10-l pb10-l">
-                                    <h1 className={`${Spirit.h1} darkgrey` }>{post.frontmatter.title}</h1>
+                                <article className="flex-auto pa5 pa8-m pa15-l pt10-ns pb10-ns pt10-l pb10-l relative">
+                                    <div className="flex content-between items-baseline justify-between no-wrap">
+                                        <h1 className={`${Spirit.h1} darkgrey`}>{post.frontmatter.title}</h1>
+                                        {githubLink &&
+                                            <a href={githubLink}
+                                                className="link no-underline midgrey flex-l dn items-start f8 absolute top-10 right-8 o-80 glow"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
+                                                <Icon name="pencil" className="w3 h3 fill-midgrey db pr2 o-80" />
+                                                Edit on GitHub
+                                            </a>
+                                        }
+                                    </div>
                                     <section
                                         className="post-content external-scripts"
                                         dangerouslySetInnerHTML={{ __html: post.html } }
